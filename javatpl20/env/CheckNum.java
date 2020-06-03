e3660d3586020e7e7a01939e6d2a88f0a83d37f3
Hiroshi Igaki <github@musictale.net>
2020-06-03 09:32:18 +0900

    update CheckNum.java

--- javatpl20/env/CheckNum.java
+++ javatpl20/env/CheckNum.java
@@ -3,6 +3,6 @@ import tplparser.num.Parser;
 public class CheckNum {
   public static void main(String[] args) {
     System.out.println(new Parser("12").number());
-    System.out.println(new Parser("121").number2());
+    System.out.println(new Parser("121").number());
   }
 }
